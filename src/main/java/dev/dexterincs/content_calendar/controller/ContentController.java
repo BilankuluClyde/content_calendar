070853fa2044976e114d3cb9f9e68da27ffec5fe
BilankuluClyde <mpfunobilac@gmail.com>
2025-01-19 20:02:10 +0200

    Adding CRUD Functionality

--- src/main/java/dev/dexterincs/content_calendar/controller/ContentController.java
+++ src/main/java/dev/dexterincs/content_calendar/controller/ContentController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -52,11 +54,17 @@ public class ContentController {
         contentCollectionRepository.save(content);
     }
 
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public void update(@RequestBody Content conten, @PathVariable Integer id)
+    public void update(@RequestBody Content content, @PathVariable Integer id)
     {
         if (!contentCollectionRepository.existsById(id))
-        throw new ResponseStatus Excpection
+        {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found");
+        }
+        
+        contentCollectionRepository.save(content); 
     }
+
+    
 }
